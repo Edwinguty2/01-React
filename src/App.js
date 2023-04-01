7d3586d46c8ec45b168aaa80b585f7b41ca9eb09
EdwinGuty <edwingutrou@unisabana.edu.co>
2023-04-01 17:51:41 -0500

    Creado el componente con tres cursos por defecto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+export const CouserList =[{
+    id:"001",
+    materia:"Calculo diferencial",
+    descripcion: "Esta materia de tres creditos desarrolla las habilidades logicas en funciones"
+},
+{
+    id:"002",
+    materia:"Fisica Mecanica",
+    descripcion: "Esta materia de tres creditos, fundamenta las leyes de Newton"
+},
+{
+    id:"003",
+    materia:"Programación orientada a objetos",
+    descripcion: "Esta materia de tres creditos,desarrolla la habilidad de 'Objetos' en programación"
+}
+]
